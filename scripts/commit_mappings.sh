b843c9f5c320ed463f22d46911ebe723546cd701
Mason Pomeroy <mason.pomeroy@hotmail.com>
2025-11-20 19:38:25 -0500

    Add quick script to push new conversation mappings

---
+++ scripts/commit_mappings.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script to commit and push conversation_mappings.json
+
+#echo "Adding conversation_mappings.json to git..."
+git add conversation_mappings.json
+
+#echo "Committing changes..."
+git commit -m "Update conversation mappings"
+
+#echo "Pushing to remote repository..."
+git push
+
+#echo "Done!"
+
